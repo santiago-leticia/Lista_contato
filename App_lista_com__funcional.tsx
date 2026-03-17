f7c02b524e249ceb77d29d4aa3b35f687e11bc42
santiago-leticia <santiagoleticia68@gmail.com>
2026-03-17 18:08:13 -0300

    first commit

--- App_lista_com__funcional.tsx
+++ App_lista_com__funcional.tsx
@@ -1,6 +1,6 @@
 import { ReactElement, useEffect, useLayoutEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Button, FlatList, FlatListProps, ListRenderItemInfo, ScrollView, StyleSheet, Text, TextInput, ToastAndroid, useColorScheme, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, ToastAndroid, useColorScheme, useWindowDimensions, View, ScrollView } from 'react-native';
 import { AntDesign as Icons } from '@expo/vector-icons';
 
 
@@ -11,33 +11,17 @@ interface Contato {
   email : string;
 }
 
-//essa forma de exibir os dados esta muito interna e vamos extadir
-const ContatoDetalhes = ( props : ListRenderItemInfo<Contato> ): ReactElement => {
-  return (
-    <View>
-        <Text>{props.item.nome}</Text>
-        <Text>{props.item.telefone}</Text>
-        <Text>{props.item.email}</Text>
-    </View>
-  )
-}
-
 export default function App() {
   const colorScheme = useColorScheme();
   const [lista, setLista] = useState<Contato[]>([
     {id: 1, nome: "Joao Silva", telefone: "(11) 1111-1111", email: "joao@teste.com"},
     {id: 2, nome: "Maria Silva", telefone: "(11) 2222-2222", email: "maria@teste.com"},
-    {id: 3, nome: "Jose Santos", telefone: "(13) 3333-3333", email: "jose@teste.com"},
-    {id: 4, nome: "Joao Silva", telefone: "(11) 1111-1111", email: "joao@teste.com"},
-    {id: 5, nome: "Maria Silva", telefone: "(11) 2222-2222", email: "maria@teste.com"},
-    {id: 6, nome: "Jose Santos", telefone: "(13) 3333-3333", email: "jose@teste.com"},
-    {id: 7, nome: "Joao Silva", telefone: "(11) 1111-1111", email: "joao@teste.com"},
-    {id: 8, nome: "Maria Silva", telefone: "(11) 2222-2222", email: "maria@teste.com"},
-    {id: 9, nome: "Jose Santos", telefone: "(13) 3333-3333", email: "jose@teste.com"}        
+    {id: 3, nome: "Jose Santos", telefone: "(13) 3333-3333", email: "jose@teste.com"}
   ]);  
   const [nome, setNome] = useState<string>("");
   const [telefone, setTelefone] = useState<string>("");
   const [email, setEmail] = useState<string>("");
+  // const [lista, setLista] = useState<Array<Contato>>([]);
   const [isDark, setDark] = useState<boolean>(
     colorScheme == "dark" ? true : false
   );
@@ -49,12 +33,71 @@ export default function App() {
   const iconColor = isDark ? "white" : "black";
   const iconName = isDark ? "sun" : "moon";
 
-  const listaFiltrada = lista
+  // Transformar a lista de objetos em elementos visuais
+  // const listaVisual = [];
+  // for (let i = 0; i < lista.length; i++) { 
+  //   const objContato = lista[i];
+  //   listaVisual.push( 
+  //     (<View>
+  //         <Text>{objContato.nome}</Text>
+  //         <Text>{objContato.telefone}</Text>
+  //         <Text>{objContato.email}</Text>
+  //       </View>)
+  //   );
+  // }
+
+  // const listaFiltrada : Contato[] = [];
+  // for (const objContato of lista) { 
+  //   if (objContato.nome.includes( filtro )) { 
+  //     listaFiltrada.push( objContato );
+  //   }
+  // }
+
+  // const listaVisual = [];
+  // for (const objContato of listaFiltrada ) { 
+  //   listaVisual.push( 
+  //     (<View>
+  //         <Text>{objContato.nome}</Text>
+  //         <Text>{objContato.telefone}</Text>
+  //         <Text>{objContato.email}</Text>
+  //       </View>)
+  //   );
+  // }
+
+  //const listaVisual : ReactElement[] = [];
+  //essa função de filtro é chamada a cada renderização, ou seja, a cada mudança de estado
+  //e ela é responsável por criar a lista de elementos visuais a partir da lista de contatos, filtrando os contatos que possuem o nome que inclui o filtro
+  
+  //a gente fez pegar a lista de contatos e filtrar ela, ou seja, criar uma nova lista apenas com os contatos que possuem o nome que inclui o filtro, e depois a gente transformou essa lista filtrada em elementos visuais
+  //vai receber cada objetos e vai retonar true ou false, se retornar true ele vai incluir o objeto na nova lista, se retornar false ele vai excluir o objeto da nova lista
+  /*const listaFiltrada = lista.filter(  (objContato : Contato, idx : number) => {
+    return objContato.nome.includes( filtro )
+  } )
+
+  const listaVisual= listaFiltrada.map( (objContato : Contato, idx : number) => {
+    return (
+      <View>
+        <Text>{objContato.nome}</Text>
+        <Text>{objContato.telefone}</Text>
+        <Text>{objContato.email}</Text>
+      </View>
+    )
+  })*/
+   const listaVisual = lista
   .filter(  (objContato : Contato, idx : number) => {
     return objContato.nome.includes( filtro )
-  } );
-
-
+  } )
+  .map( (objContato : Contato, idx : number) => { return (
+      <View key={`contato-${objContato.id}`}>
+          <Text>{objContato.nome}</Text>
+          <Text>{objContato.telefone}</Text>
+          <Text>{objContato.email}</Text>
+      </View>)
+  } )  
+  //java e kotli fazem isso com o stream, ou seja, a gente pega a lista de contatos e transforma ela em um stream, e depois a gente aplica o filtro e o map nesse stream, e depois a gente transforma esse stream em uma lista de elementos visuais
+  //mas o typeScript não tem stream, então a gente tem que fazer isso com as funções de array mesmo, ou seja, a gente pega a lista de contatos e aplica o filtro e o map diretamente nela, sem precisar transformar ela em um stream antes
+  
+  //o flacklist, nao vai mostra uma lista enorme para a gente ver mas sim algo pequeno e bom visualmente
 
   return (
     <View style={estiloAtual.main}>
@@ -108,12 +151,7 @@ export default function App() {
             placeholderTextColor = {placeHolderColor}/>
           <Text> Lista </Text>
           <ScrollView>
-            <FlatList data = {listaFiltrada}
-              renderItem = { ContatoDetalhes }
-              keyExtractor = { 
-              (contato: Contato) => `contato-${contato.id}`
-              }
-              />
+            {listaVisual}
           </ScrollView>
       </View>
     </View>
